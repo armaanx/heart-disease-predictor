d1b000936e4dad2a96263426478eef8a3293617b
Armaan Mishra <armaanmishra48@gmail.com>
2023-10-22 01:45:07 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,146 @@
-import Image from 'next/image'
-
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex items-center justify-center lg:h-screen'>
+      <div className='flex flex-col gap-3 p-11 pt-16 lg:grid lg:grid-flow-row lg:grid-cols-2 lg:justify-center lg:items-end lg:gap-6'>
+        <div>
+          <Label htmlFor='age'>Age</Label>
+          <Input id='age' placeholder='Input' type='number' min={1} max={100}/> 
         </div>
+
+        <div>
+        <Label htmlFor='sex'>Sex</Label>
+          <Select>
+            <SelectTrigger id='sex'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='0'>Male</SelectItem>
+              <SelectItem value='1'>Female</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+        <Label htmlFor='cp'>Chest Pain Type (select 'Asymptomatic' if no pain)</Label>
+          <Select>
+            <SelectTrigger id='cp'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='0'>Typical Angina</SelectItem>
+              <SelectItem value='1'>Atypical Angina</SelectItem>
+              <SelectItem value='2'>Non-anginal Pain</SelectItem>
+              <SelectItem value='3'>Asymptomatic</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <Label htmlFor='trestbps'>Resting Blood Pressure (mmHG)</Label>
+          <Input placeholder='Input' id='trestbps' type='number' min={0} max={220}/>
+        </div>
+
+        <div>
+          <Label htmlFor='chol'>Serum Cholesterol (mg/dl)</Label>
+          <Input placeholder='Input' id='chol' type='number' min={0} max={500}/>
+        </div>
+
+        <div>
+        <Label htmlFor='fbs'>Fasting Blood Sugar (mg/dl)</Label>
+          <Select>
+            <SelectTrigger id='fbs'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='1'>Over 120 mg/dl</SelectItem>
+              <SelectItem value='0'>Under 120 mg/dl</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+        <Label htmlFor='restecg'>Resting ElectroCardioGraphic (ECG) Results</Label>
+          <Select>
+            <SelectTrigger id='restecg'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='0'>Normal</SelectItem>
+              <SelectItem value='1'>ST-T wave abnormality</SelectItem>
+              <SelectItem value='2'>Left Ventricular Hyperthrophy</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <Label htmlFor='thalach'>Maximum Heart Rate Achieved (bpm)</Label>
+          <Input placeholder='Input' id='thalach' type='number' min={0} max={300}/>
+        </div>
+
+        <div>
+        <Label htmlFor='exang'>Do you have Exercise Induced Angina?</Label>
+          <Select>
+            <SelectTrigger id='exang'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='1'>Yes</SelectItem>
+              <SelectItem value='0'>No</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <Label htmlFor='oldpeak'>Oldpeak (ST depression induced by exercise relative to rest)</Label>
+          <Input placeholder='Input' id='oldpeak' type='number' min={0} max={7} step={0.10}/>
+        </div>
+
+        <div>
+        <Label htmlFor='slope'>Slope (Peak exercise ST segment)</Label>
+          <Select>
+            <SelectTrigger id='slope'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='0'>Unsloping</SelectItem>
+              <SelectItem value='1'>Flat</SelectItem>
+              <SelectItem value='2'>Downsloping</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <Label htmlFor='ca'>Number of Major Vessels Colored by Flourosopy</Label>
+          <Input placeholder='Input' id='ca' type='number' min={0} max={3} step={1}/>
+        </div>
+
+        <div>
+        <Label htmlFor='thal'>Thalassemia</Label>
+          <Select>
+            <SelectTrigger id='thal'>
+              <SelectValue placeholder='Select'/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='3'>Normal</SelectItem>
+              <SelectItem value='6'>Fixed Defect</SelectItem>
+              <SelectItem value='7'>Reversible Defect</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <Button>See Results</Button>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
